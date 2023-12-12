eb962b5f4cd4756d8903447897677963fb4a3b8d
zedr0n <zedr0n@protonmail.com>
2023-12-12 22:23:56 +0000

    remove more android

--- qrcode.js
+++ qrcode.js
@@ -154,24 +154,7 @@ var QRCode;
 	function _isSupportCanvas() {
 		return typeof CanvasRenderingContext2D != "undefined";
 	}
-	
-	// android 2.x doesn't support Data-URI spec
-	function _getAndroid() {
-		var android = false;
-		var sAgent = navigator.userAgent;
 		
-		if (/android/i.test(sAgent)) { // android
-			android = true;
-			var aMat = sAgent.toString().match(/android ([0-9]\.[0-9])/i);
-			
-			if (aMat && aMat[1]) {
-				android = parseFloat(aMat[1]);
-			}
-		}
-		
-		return android;
-	}
-	
 	var svgDrawer = (function() {
 
 		var Drawing = function (el, htOption) {
